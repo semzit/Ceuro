9d9d449182258f044d80c7066bb1a3cf22f032e1
Semo Zitouni <zitouni.skz@gmail.com>
2026-05-11 23:29:18 -0500

    restarting

--- ceuro.c
+++ ceuro.c
@@ -1,144 +1,69 @@
 #include <stdio.h> 
 
-typedef enum {
-  STANDARD_DOT_PRODUCT, 
-  NON_EUCLIDEAN_DOT_PRODUCT, 
-}InnerProducts ; 
-
-typedef enum {
-  RELU, 
-  SIGMOID,
-}Activations; 
-
 typedef struct {
-	char* name ;  
-	int* dimension;
-  int layers; 
-  Activations activation; 
-}Network; 
+  Neuron* from; 
+  Neuron* to; 
+  double weight; 
+}Connection; 
 
-typedef struct {
-  int bias; 
-  int* connections;
-  Activations activation; 
+typedef struct{
+  double bias ; 
 }Neuron;
 
-typedef struct {
-  InnerProducts innerProduct; 
-  float weight;  
-  int* from;
-  int* to;
-}Connection;
+typdef Connection* Layer; 
 
-typedef struct {
-  float guess;
-  float error;  
-}Results;
+typedef struct{
+  int* dimensions; 
+}Network; 
 
-typedef struct {
-  int* input;  
-}Input;
-
-int multiply(int weight, int input){
-  return weight * intput ; 
+void backPropagate(){
+  // calculate the error 
+  //  change in weight = lr * error term * output
+  //    error term : 
+  //      if in last layer : error rate = output(1-output)(target - output)
+  //      if in hidden layer : error rate = output (1-output)  (weight * (next error rate))
 }
 
-int activate(int in){
-  if (in > 0.5){
-    return 1; 
-  }else {
-    return 0; 
-  }
+void forwardPropagate(){
+  // take input and feed through network
+  //    for every layer : 
+  //      multiply by layer 
+  //      input to nonlinearity
 }
 
-void feedForward(Network *net , Input *in){
-  if (sizeof(*in.input) < *net.dimension[0]){
-    exit(EXIT_FAILUR); 
-  }
-  
-  for (int i = 0 ; i < layers ; i++){
-    for (int j = 0 ; j < net.dimensions[i] ; j++){
-        if (i = 0){
-          weight = *net.
-          multiplied = multiply(weight, in); 
-          out = activate(multiplied); 
-        }
-
+Network createNetwork(int* dimensions){
+  // create neurons 
+  for (int i = 0; i < sizeof(*dimensions) ; i++){
+    for (int j = 0; j < dimensions[i] ; j++){
 
     }
   }
 
-
-}
-
-// delta = target - output
-// gradient = delta * outputTransferFunciton derivative
-void calcOutputGrad(){}
-
-// sum through the weights in next layer multiplied by their gradient 
-void calcDOW(Layer nextLayer){}
-
-// dow = 
-// gradient = delta * outputTransferFunciton
-void calcHiddenGrad(){}
-
-void backprop(){
-  // calculate output gradients
+  // create connections 
   
-  // calculate the hidden layer gradients 
+  //create layers  
   
-  // update neuron weights 
 }
 
-void getResults(){
 
-}
+int int main(int argc, char *argv[]){
+  // Get epochs from user 
+  int epochs = argv[1];
 
-void createNetwork(Network *net){
-  // For each layer in the neural network add neurons
-  for (int i = 0; i < net.layers; i++){
-    for (int j = 0 ; j < net.dimension[i]; j++){
-      // create neuron 
-      Neuron* ner =  malloc(sizeof(Neuron));
-      
-      ner->bias = rand ; 
-      ner->activation = net.activation; 
-     // create connections
-      Connection connections[net.dimensions[j+1]] ; 
-      if (i < net.layers-1){ // dont create any connection for the last layer 
-          for (int k = 0; k < net.dimension[j+1]; k++){ 
-            connections[k].innerProduct = innerProducts.STANDARD_DOT_PRODUCT, 
-            connections[k].weight = rand, 
-            connections[k].from = &ner, 
-            connections[k].to = &ner + net.dimensions[j+1],  // to neuron should be that far away
-          }  
-        }
-      }
-      ner->connections = connections ;  // update connection
-    }
-  }
-}
-
-int main(void){
-
-	// enter neural network dimensions 
-  int dimensions[] = {2, 3, 4, 5}; 
-  
-	// create a neural network using dimensions 
-	Network net = {
-		.name = "first net", 
-    .dimension = dimensions, 
-    .layers = 4
-	};
-
-  // 10 epochs 
-  for (int i = 0 ; i < 10 ; i++){
-    int results = net.feedForward(); 
-    net.backprop(results); 
+  // Get dimensions from user 
+  int dimensions[argc-2]; 
+  for (int i = 0; i < argc ; i++){
+    dimensions[i] = *argv[i+1]; 
   }
 
+  // create a network
+  Network* net = createNetwork(dimensions); 
 
-  int results [] = net.getResults(); 
-  printf("%s\n");
+  for (int i = 0 ; i < epochs ; i++){
+    // forward propagate
+    output = forwardPropagate(); 
 
+    // back propagate
+    backPropagate(output); 
+  }
 }
